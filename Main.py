5181368eb00824d4d25e3166a6d0762a3e358575
ostap <43792052+OstapB@users.noreply.github.com>
2019-05-23 13:55:00 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,11 +3,11 @@ from MuseumSculpture import MuseumSculpture
 
 def main():
     zeus_sculpture = MuseumSculpture(1234, "Zeus_Sculpture", "Wax", 150, 100000)
-    ahilles_sculpture = MuseumSculpture(1310, "Ahilles_Sculpture", "Wax", 110, 120000)
+    achilles_sculpture = MuseumSculpture(1310, "Achilles_Sculpture", "Wax", 110, 120000)
     lenin_sculpture = MuseumSculpture(1946, "Lenin_Sculpture", "Clay", 200)
 
     print(zeus_sculpture)
-    print(ahilles_sculpture)
+    print(achilles_sculpture)
     print(lenin_sculpture)
     print(MuseumSculpture.amount_of_sculptures)
 
